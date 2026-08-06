7b2840650ffbc380a47e73c9950095ed1e091c46
Madhushika <jayawansa123s@gmail.com>
2026-08-06 18:22:29 +0530

    Add SQL commands to create database and schema for Airbnb analytics

--- sql/01_create_environment.sql
+++ sql/01_create_environment.sql
@@ -10,3 +10,19 @@ WITH
     AUTO_RESUME = TRUE;
 
 USE WAREHOUSE AIRBNB_WH;
+
+-- ==========================================
+-- Create Database
+-- ==========================================
+
+CREATE DATABASE AIRBNB_ANALYTICS_DB;
+
+USE DATABASE AIRBNB_ANALYTICS_DB;
+
+-- ==========================================
+-- Create Schema
+-- ==========================================
+
+CREATE SCHEMA STAR_SCHEMA;
+
+USE SCHEMA STAR_SCHEMA;
