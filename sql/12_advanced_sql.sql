7cd6eb4f492130af567a0a31218657f914184f9c
Madhushika <jayawansa123s@gmail.com>
2026-08-11 22:57:37 +0530

    Add SQL analysis for identifying hosts with more listings than average per host

--- sql/12_advanced_sql.sql
+++ sql/12_advanced_sql.sql
@@ -29,3 +29,32 @@ WHERE price > (
     SELECT AVG(price)
     FROM Fact_Listings
 );
+
+--==============================================================================
+--Step-3
+--subquery inside another query
+-- Business Question
+--Which hosts have more listings than the average number of listings per host?
+--===========================================================================================
+
+SELECT
+    dh.host_id,
+    dh.host_name,
+    COUNT(*) AS Total_Listings
+FROM Fact_Listings fl
+JOIN Dim_Host dh
+    ON fl.host_key = dh.host_key
+GROUP BY
+    dh.host_id,
+    dh.host_name
+HAVING COUNT(*) > (
+    SELECT AVG(Total_Listings)
+    FROM (
+        SELECT
+            host_key,
+            COUNT(*) AS Total_Listings
+        FROM Fact_Listings
+        GROUP BY host_key
+    )
+)
+ORDER BY Total_Listings DESC;
